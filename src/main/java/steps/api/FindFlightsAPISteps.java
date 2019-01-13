f26ed86e5301407b33e133ef232c0eceaab0c78f
Venugopal Rao. Donekal <vdonekal@oradev.oraclecorp.com>
2019-01-13 14:59:34 +0530

    Changes to POM.xml

--- src/main/java/steps/api/FindFlightsAPISteps.java
+++ src/main/java/steps/api/FindFlightsAPISteps.java
@@ -5,7 +5,8 @@ import net.thucydides.core.annotations.Step;
 import static net.serenitybdd.rest.SerenityRest.given;
 
 public class FindFlightsAPISteps {
-    @Step
+    @SuppressWarnings("deprecation")
+	@Step
     public void findFlights() {
         given().parameters("fromPort", "Paris", "toPort", "Buenos Aires").when().post("http://blazedemo.com/reserve.php");
     }
