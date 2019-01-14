b6dd47e645666d129603a6bfb062255cf361c530
dvenugopalrao <dvenugopalrao@gmail.com>
2019-01-14 19:31:43 +0530

    Adding the file pipeline script

---
+++ jenkins-pipelines/test.groovy
@@ -0,0 +1,22 @@
+#!groovy
+node {
+    stage('Git checkout') { // for display purposes
+        git 'https://github.com/dvenugopalrao/automation-pipeline_venu.git'
+    }
+     stage('UI') {
+        try {
+            sh "mvn clean verify -Dtags='type:UI'"
+        } catch (err) {
+
+        } finally {
+            publishHTML (target: [
+                    reportDir: 'target/site/serenity',
+                    reportFiles: 'index.html',
+                    reportName: "UI tests report"
+            ])
+        }
+    }
+    stage('Results') {
+        junit '**/target/failsafe-reports/*.xml'
+    }
+}
